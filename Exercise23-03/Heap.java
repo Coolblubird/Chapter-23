3874ff7a5272426da4f4156915a493a2319d0dfe
Coolblubird <80004413+Coolblubird@users.noreply.github.com>
2021-07-07 10:48:24 -0600

    init commit

---
+++ Exercise23-03/Heap.java
@@ -0,0 +1,124 @@
+/*
+Author: Jordan Ashe
+Date: 07-7-21
+
+Description: the original was made by Y. Daniel Yiang.
+ */
+public class Heap<E extends Comparable<E>> {
+  private java.util.ArrayList<E> list = new java.util.ArrayList<>();
+
+  /** Create a default heap */
+  public Heap() {
+  }
+
+  /** Create a heap from an array of objects */
+  public Heap(E[] objects) {
+    for (int i = 0; i < objects.length; i++)
+      add(objects[i]);
+  }
+
+  /** Add a new object into the heap */
+  public void add(E newObject) {
+    list.add(newObject); // Append to the heap
+    
+    int currentIndex = 0;
+    while (currentIndex < list.size()) {
+      int tiny = currentIndex; // Initialize smallest as root
+      int l = 2 * currentIndex + 1; // left = 2*i + 1
+      int r = 2 * currentIndex + 2; // right = 2*i + 2
+      boolean switch1 = false;
+      
+      // If left child is smaller than root
+      if (l < list.size()-1 && list.get(l).compareTo(list.get(tiny)) < 0){
+        switch1 = true;
+        tiny = l;
+      }
+      
+      //if right smaller still
+      if (r < list.size()-1 && list.get(r).compareTo(list.get(tiny)) < 0)
+        tiny = r;
+      
+      // Swap if the current object is lnot the current one
+      if (tiny != currentIndex) {
+          E temp = list.get(tiny); 
+          list.set(tiny, list.get(currentIndex));
+          list.set(currentIndex, temp);
+          currentIndex = tiny;
+      }
+      else
+        break; // the tree is a heap now
+        
+      //ensureSecurity(currentIndex, switch1);
+    } 
+  }
+
+  /** Remove the root from the heap */
+  public E remove() {
+    if (list.size() == 0) return null;
+
+    E removedObject = list.get(0);
+    list.set(0, list.get(list.size() - 1));
+    list.remove(list.size() - 1);
+
+    int currentIndex = 0;
+    while (currentIndex < list.size()) {
+      int tiny = currentIndex; // Initialize smallest as root
+      int l = 2 * currentIndex + 1; // left = 2*i + 1
+      int r = 2 * currentIndex + 2; // right = 2*i + 2
+      boolean switch1 = false;
+      
+      // If left child is smaller than root
+      if (l < list.size()-1 && list.get(l).compareTo(list.get(tiny)) < 0){
+        switch1 = true;
+        tiny = l;
+      }
+      
+      //if right smaller still
+      if (r < list.size()-1 && list.get(r).compareTo(list.get(tiny)) < 0)
+        tiny = r;
+      
+      // Swap if the current object is less than the other
+      if (tiny != currentIndex) {
+          E temp = list.get(tiny); 
+          list.set(tiny, list.get(currentIndex));
+          list.set(currentIndex, temp);
+          currentIndex = tiny;
+      }
+      else
+        break; // the tree is a heap now
+        
+      //ensureSecurity(currentIndex, switch1);
+    } 
+    
+    return removedObject;
+  }
+
+  //fixes the l and r being out of sequencial order (?)
+  /*public void ensureSecurity(int cI, boolean isLeft){
+    if (isLeft){
+      if (list.get(cI).compareTo(list.get(cI+1)) < 0){
+        E temp = list.get(cI); 
+        list.set(cI, list.get(cI+1));
+        list.set(cI+1, temp);
+      }
+    }
+    else{
+      if (list.get(cI-1).compareTo(list.get(cI)) < 0){
+        E temp = list.get(cI); 
+        list.set(cI, list.get(cI-1));
+        list.set(cI-1, temp);
+      }
+    }
+  }*/
+
+
+  /** Get the number of nodes in the tree */
+  public int getSize() {
+    return list.size();
+  }
+  
+  /** Return true if heap is empty */
+  public boolean isEmpty() {
+    return list.size() == 0;
+  }
+}
---
+++ Exercise23-07/Exercise23_03.java
@@ -0,0 +1,122 @@
+/*
+Author: Jordan Ashe 
+Date: 07-7-21
+
+Description: This is the only file I've modified so I didn't see it fair to place my name in the other ones. I am not sure what this other error is as it cannot happen on any of the computers in the lab, whether my friend does it or me.
+*/
+import java.util.Comparator;
+
+public class Exercise23_03 {
+  public static void main(String[] args) {
+    Integer[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+    quickSort(list);
+    for (int i = 0; i < list.length; i++) {
+      System.out.print(list[i] + " ");
+    }
+  
+    System.out.println();
+    Circle[] list1 = {new Circle(2), new Circle(3), new Circle(2),new Circle(5), new Circle(6), new Circle(1), new Circle(2),new Circle(3), new Circle(14), new Circle(12)};
+    quickSort(list1, new GeometricObjectComparator());
+    for (int i = 0; i < list1.length; i++) {
+      System.out.println(list1[i] + " ");
+    }
+  }
+
+  public static <E extends Comparable<E>> void quickSort(E[] list){
+    quickSort(list,0,list.length-1);
+  }
+  
+  public static <E extends Comparable<E>> void quickSort(E[] list,int first, int last){
+    if (last > first) {
+      int index = part(list, first,last);
+      quickSort(list,first,index-1);
+      quickSort(list,index+1,last);
+    }
+  }
+  
+  public static <E extends Comparable<E>> int part(E[] list, int first, int last) {
+    E pivot = list[first];
+    int low = first+1; 
+    int high = last; //exclude the pivot
+    
+    while (high > low){
+      //meetup in the middle
+      while (low <= high && list[low].compareTo(pivot) <= 0){
+        low++;
+      }
+      
+      while (low <= high && list[high].compareTo(pivot) > 0){
+        high--; 
+      }
+      
+      //swap
+      if (high > low){
+        E temp = list[high];
+        list[high] = list[low];
+        list[low] = temp;
+      }
+    }
+    
+    while (high > first && list[high].compareTo(pivot) >= 0){
+      high--;
+    }
+    
+    if (pivot.compareTo(list[high]) > 0){
+      list[first]=list[high];
+      list[high] = pivot;
+      return high;
+    }
+    else{
+      return first;
+    }
+  }
+
+  public static <E> void quickSort(E[] list, Comparator<E> comparator){
+    quickSort(list,0,list.length-1,comparator);
+  }
+  
+  public static <E> int part(E[] list, int first, int last,Comparator<E> comparator) {
+    E pivot = list[first];
+    int low = first+1; 
+    int high = last; //exclude the pivot
+    
+    while (high > low){
+      //meetup in the middle
+      while (low <= high && comparator.compare(list[low],pivot) <= 0){
+        low++;
+      }
+      
+      while (low <= high && comparator.compare(list[high],pivot) > 0){
+        high--; 
+      }
+      
+      //swap
+      if (high > low){
+        E temp = list[high];
+        list[high] = list[low];
+        list[low] = temp;
+      }
+    }
+    
+    while (high > first && comparator.compare(list[high],pivot) >= 0){
+      high--;
+    }
+    
+    if (comparator.compare(pivot,list[high]) > 0){
+      list[first]=list[high];
+      list[high] = pivot;
+      return high;
+    }
+    else{
+      return first;
+    }
+  }
+  
+  public static <E> void quickSort(E[] list, int first, int last, Comparator comparator){
+    if (last > first) {
+      int index = part(list, first, last, comparator);
+      quickSort(list,first,index-1,comparator);
+      quickSort(list,index+1,last,comparator);
+     }
+  }
+}
